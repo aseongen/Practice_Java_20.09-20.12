6809a3385b2412008ac694e9af881e0cf6726fe9
se <aseongen12@gmail.com>
2020-09-23 18:56:46 +0900

    SEP_23
    secondProject

---
+++ flowerPot/src/main/webapp/resources/sql/f_attachFile.sql
@@ -0,0 +1,17 @@
+create table f_attachFile (
+	atno number(30),
+	cno number(30) default 0,
+	mgno number(30) default 0,
+	uuidName varchar2(200) not null,
+	originalFileName varchar2(200) not null,
+	uploadFolderPath varchar2(200) not null,
+	mappingURL varchar2(200) not null,
+	realName varchar2(200) not null,
+	primary key (atno)
+	-- constraint  foreign key (cno) references f_cosmetic(cno)   외래키 제약조건
+	-- constraint  foreign key (cno) references f_cosmeticReview(cno)
+);
+create sequence atno_seq start with 1 increment by 1;
+
+drop table f_attachFile;
+select * from f_attachFile;
---
+++ flowerPot/src/main/webapp/resources/sql/f_member.sql
@@ -0,0 +1,33 @@
+create table f_member (
+    mno number(30),
+    id varchar2(100) not null,
+    password varchar2(100),
+    name varchar2(20) not null,
+    nickname varchar2(50),
+    address varchar2(100),
+    email varchar2(100),
+    gender varchar2(2),    
+    birth varchar2(10),
+    member_rank varchar2(15),
+    enabled char(1) default '1',
+    regdate date,
+    primary key(mno),
+    unique(id),
+    unique(email)
+);
+-- 시퀀스
+create sequence mno_seq increment by 1 start with 1 minvalue 1;
+
+---------------------------------------------------------------------------------------------------------------------------------------------------
+select mno_seq.nextval from dual;
+select * from f_member;
+drop table f_member;
+delete from f_member where name='윤신영';
+
+insert into f_member (mno,id,password,name,nickname,address,email,gender,birth,member_rank,authority ) 
+values (mno_seq.nextval,'phantom1','tlsdud5089','윤신영','윤신영','주소','phantom_ysy@naver.com','M','11-29','골드','작성자');
+
+insert into f_member (mno,id,password,name,nickname,address,email,gender,birth,member_rank,authority ) 
+values (mno_seq.nextval,'admin','admin','윤신영','윤신영','주소','phantom_ysy@naver.com','M','11-29','골드','일반회원');
+
+select id, password from f_member where id = 'user0';
